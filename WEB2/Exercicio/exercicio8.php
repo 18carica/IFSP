96c57ef4e4a0266391732a01b5d6d543c698b524
LEANDRO TORRES MOCELIN <122543349+18carica@users.noreply.github.com>
2024-02-27 22:39:20 -0300

    Update Exercicio8.php

--- WEB2/Exercicio/exercicio8.php
+++ WEB2/Exercicio/exercicio8.php
@@ -11,40 +11,43 @@
     
     <h3>Exercício 8</h3>
     <?php
-    function verificarAulaDW2($diaSemana) {
-        // Converter para minúsculas para garantir que a comparação não seja sensível a maiúsculas
-        $diaSemana = strtolower($diaSemana);
 
-        // Array associativo que mapeia o dia da semana para se há aula de DW2 ou não
-        $aulasDW2 = array(
-            "segunda-feira" => "Neste dia não tem aula de DW2",
-            "terça-feira" => "Neste dia tem aula de DW2",
-            "quarta-feira" => "Neste dia não tem aula de DW2",
-            "quinta-feira" => "Neste dia tem aula de DW2",
-            "sexta-feira" => "Neste dia não tem aula de DW2",
-            "sábado" => "Neste dia não tem aula de DW2",
-            "domingo" => "Neste dia não tem aula de DW2"
-        );
+function verificarAulaDW2($diaSemana) {
+    // Converter o dia da semana para minúsculas para garantir correspondência
+    $diaSemana = strtolower($diaSemana);
 
-        // Verificar se o dia informado está no array
-        if (array_key_exists($diaSemana, $aulasDW2)) {
-            echo $aulasDW2[$diaSemana]; // Imprimir se há aula de DW2 ou não
-        } else {
-            echo "Dia da semana inválido."; // Caso o dia informado não seja reconhecido
-        }
+    // Array associativo que mapeia o dia da semana para a disponibilidade de aula de DW2
+    $aulasDW2 = array(
+        "segunda-feira" => "Neste dia tem aula de DW2",
+        "terça-feira" => "Neste dia tem aula de DW2",
+        "quarta-feira" => "Neste dia não tem aula de DW2",
+        "quinta-feira" => "Neste dia tem aula de DW2",
+        "sexta-feira" => "Neste dia não tem aula de DW2",
+        "sábado" => "Neste dia não tem aula de DW2",
+        "domingo" => "Neste dia não tem aula de DW2"
+    );
+
+    // Verifica se o dia da semana está no array
+    if (array_key_exists($diaSemana, $aulasDW2)) {
+        echo $aulasDW2[$diaSemana]; // Imprime a mensagem correspondente ao dia
+    } else {
+        echo "Dia da semana inválido."; // Se o dia da semana informado não estiver no array
     }
+}
 
-    // Exemplos de uso da função
-    verificarAulaDW2("segunda-feira");
-    echo "<br>";
-    verificarAulaDW2("quarta-feira");
-    echo "<br>";
-    verificarAulaDW2("sábado");
-    echo "<br>";
-    verificarAulaDW2("terça-feira");
-    echo "<br>";
-    verificarAulaDW2("segunda");
-    echo "<br>";
+// Exemplos de uso da função
+verificarAulaDW2("segunda-feira");
+echo "<br>";
+verificarAulaDW2("quarta-feira");
+echo "<br>";
+verificarAulaDW2("sábado");
+echo "<br>";
+verificarAulaDW2("terça-feira");
+echo "<br>";
+verificarAulaDW2("segunda");
+echo "<br>";
+
 ?>
+
 </body>
 </html>
